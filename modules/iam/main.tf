0400369da8ad10b25265b713bce33e0ed3d9be56
VishalUmbarkar <vishalumbarkar11@gmail.com>
2025-04-04 12:18:52 +0530

    add iam module

---
+++ modules/iam/main.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = var.execution_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
+  name       = "ecs_execution_policy_attachment"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "secrets_manager_access" {
+  name = "secrets_manager_access"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["secretsmanager:GetSecretValue"]
+      Resource = var.secrets_arn
+    }]
+  })
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = var.task_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
